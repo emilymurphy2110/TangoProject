e7fa4e9f53335cddb7b08558db3bb54e4c9bd0ea
Emily Murphy <2087350m@student.gla.ac.uk>
2020-02-06 19:35:59 +0000

    end of exercises chapter 8

--- rango/views.py
+++ rango/views.py
@@ -58,7 +58,7 @@ def add_category(request):
             form.save(commit=True)
             # now that the category is saved, we could confirm this.
             # for now, just direct the user back to the index view.
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             # the supplied form contained errors
             # just print them to the terminal
@@ -77,7 +77,7 @@ def add_page(request, category_name_slug):
 
         # you cannot add a page to a category that does not exist...
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
